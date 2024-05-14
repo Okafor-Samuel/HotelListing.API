8e280d513330bacf194e29df1e1fda69d061b314
Samuel Okafor <OkaforSam@accessbankplc.com>
2024-05-14 09:58:36 +0100

    Hotel Listing

---
+++ HotelListing.API/Data/HotelListngDbContext.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListngDbContext : DbContext
+    {
+        public HotelListngDbContext(DbContextOptions options) : base(options)
+        {
+              
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country 
+                { 
+                    Id=2,
+                    Name = "Nigeria",
+                    ShortName = "NGN"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                }
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+               new Hotel
+               {
+                   Id = 1,
+                   Name = "Sandals Resort and Spa",
+                   Address = "Negril",
+                   CountryId = 1,
+                   Rating = 4.5
+               },
+               new Hotel
+               {
+                   Id = 2,
+                   Name = "Comfort Suites",
+                   Address = "George Town",
+                   CountryId = 3,
+                   Rating = 4.3
+               },
+               new Hotel
+               {
+                   Id = 3,
+                   Name = "Grand Palldium",
+                   Address = "Nassua",
+                   CountryId = 2,
+                   Rating = 4
+               }
+               );
+        }
+    }
+}
